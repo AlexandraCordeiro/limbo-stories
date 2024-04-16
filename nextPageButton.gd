4048b2ccd44259cabb8ca8699a38043db7e41184
Alexandra <acordeiro2002@gmail.com>
2024-04-16 14:03:51 +0100

    fix scale issues, delay not fixed

--- nextPageButton.gd
+++ nextPageButton.gd
@@ -8,5 +8,6 @@ func _process(_delta):
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:	# button left == 1
-		get_tree().change_scene_to_file("res://scene.tscn")
+		pass
 		
+			
