0eae798138933bbafcf28afdfae505573fb5704e
Alexandra <acordeiro2002@gmail.com>
2024-04-29 22:40:35 +0100

    waterfall scene

--- close_button.gd
+++ close_button.gd
@@ -8,5 +8,5 @@ func _process(_delta):
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:	# button left == 1
-		get_tree().change_scene_to_file("res://scene.tscn")
+		get_tree().change_scene_to_file("res://waterfall.tscn")
 		
--- global_script.gd
+++ global_script.gd
@@ -4,6 +4,7 @@ var diary_was_found = false
 var fist_time_scene_principal = true
 var read_mode = false
 var cover = preload("res://cover.tscn").instantiate()
+var waterfall = preload("res://waterfall.tscn").instantiate()
 var first_entry = preload("res://first_entry.tscn").instantiate()
 var second_entry = preload("res://second_entry.tscn").instantiate()
 var diary_entries = {"cover": cover, "first_entry": first_entry}
