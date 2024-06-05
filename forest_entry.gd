23f6b6f2b5879f9ff0604fca26a3cbf962838df4
Bruna Leitão <brunaleitao10@gmail.com>
2024-06-05 16:47:51 +0100

    Transition to black

--- forest_entry.gd
+++ forest_entry.gd
@@ -6,4 +6,6 @@ func _on_body_entered(body):
 	if body is Player:
 		global.what_scene = global.level_sw
 		global.enteredLevel = true
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file("res://level_switcher.tscn")
